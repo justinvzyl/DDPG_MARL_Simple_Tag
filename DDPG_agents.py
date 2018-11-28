5f1f5841c040b04956fbb85c9212c35bcf0e1f4c
justinvzyl <justin.vanzyl@gmail.com>
2018-11-28 04:14:07 +0200

    DDPG Algo Implementation For Simple Tag environment 1v1

--- DDPG_agents.py
+++ DDPG_agents.py
@@ -1,139 +1,113 @@
 import torch
-import gym
 from buffer import Memory
 import utils
-from collections import deque
 from ACClasses import Actor, Critic
 from utils import OU_noise
 import torch.nn as nn
-from itertools import count
-from tensorboardX import SummaryWriter
-
-dtype= torch.float
-GAMMA = 0.99 #discount factor
-ALPHA = 1e-3 #learning rate 
-BATCH_SIZE = 16 #number of samples in batch
-REWARD_THRESHOLD = 200
-BUFFER_SIZE = 10000
-TAU = 0.001
-Total_Reward_List = deque(maxlen=100)
-mean_100 = 0
-episode_num = 0
-buffer = Memory(BUFFER_SIZE)
-env_name = 'Pendulum-v0'
-env = gym.make(env_name)
-noise = OU_noise(dt=0.05)
-writer = SummaryWriter(log_dir='./test1')
-
-MAX_STEPS = 200
 
 
-online_actor = Actor(env.observation_space.shape[0], env.action_space.shape[0])
-online_critic = Critic(env.observation_space.shape[0], env.action_space.shape[0])
-
-target_actor = Actor(env.observation_space.shape[0], env.action_space.shape[0])
-target_critic = Critic(env.observation_space.shape[0], env.action_space.shape[0])
-
-'''Copy Online Parameters to target Parameters'''
-target_actor.policy.load_state_dict(online_actor.policy.state_dict())
-target_critic.value_func.load_state_dict(online_critic.value_func.state_dict())
-
-optim = torch.optim.Adam(online_critic.value_func.parameters(),lr=ALPHA)
-
-
-'''Main Loop'''
-
+class Agent():
+    def __init__(self, name, n_o, n_a):
+        self.agent_name = name #name of the agent: 'predator' or 'prey'
+        self.dtype= torch.float
+        self.obs_dim = n_o #observation space dimensions
+        self.act_dim = n_a #action space dimensions
+        self.GAMMA = 0.99 #discount factor
+        self.ALPHA = 1e-3 #learning rate 
+        self.BATCH_SIZE = 16 #number of samples in batch
+        self.BUFFER_SIZE = 10000
+        self.TAU = 0.001
+        self.buffer = Memory(self.BUFFER_SIZE)
+        self.noise = OU_noise(dt=0.05)
+        
+        self.online_actor = Actor(self.obs_dim, self.act_dim)
+        self.online_critic = Critic(self.obs_dim, 1)
+        
+        self.target_actor = Actor(self.obs_dim, self.act_dim)
+        self.target_critic = Critic(self.obs_dim, 1)
+        
+        '''Copy Online Parameters to target Parameters'''
+        self.target_actor.policy.load_state_dict(self.online_actor.policy.state_dict())
+        self.target_critic.value_func.load_state_dict(self.online_critic.value_func.state_dict())
+        
+        self.optim = torch.optim.Adam(self.online_critic.value_func.parameters(),lr=self.ALPHA)
+        
+        self.state = None #temp holding of variables before being pushed to replay buffer. 2D tensor [[state]]
+        self.act = None #temp holding of variables before being pushed to replay buffer. 2D tensore [[action]]
     
-state_v = torch.tensor([env.reset()], dtype=dtype)
-
-for i in count():
-#    if i % 1000 == 0:
-#        noise.reset()
-    env.render()
-    #get the action using online actor network and add noise
-    action = online_actor.get_action(state_v) + noise.step()
-    #get next state, reward, done, and info by taking a step in environment
-    next_state, reward, done, _ = env.step(action.detach())
-    #convert next state and reward to tensors
-    next_state_v = torch.tensor([next_state],dtype=dtype)
-    reward_v = torch.tensor([reward],dtype=dtype)
     
-    #save the values in the replay buffer
-    buffer.push(state_v,action,reward_v,next_state_v,done)
-    state_v = next_state_v
+    def agent_get_action(self, observation):
+        assert type(observation) is torch.Tensor, 'The state is not a tensor! It is of type: %s' % (type(observation))
+        self.state = observation 
     
-
-    #if the buffer is filled up start mini batch sampling and training    
-    if len(buffer) < BUFFER_SIZE:
-        print('Filling up buffer: %.2f'%(len(buffer)/BUFFER_SIZE*100))
-    else:
-        #get a mini batch from the replay buffer
-        sample = buffer.sample(BATCH_SIZE)
-        #make the data nice
-        compressed_states, compressed_actions, compressed_next_states, compressed_rewards = utils.extract_data(sample)
-        
-        #critic network training
-        #yt=r(st,at)+γ⋅Q(st+1,μ(st+1))
-        na_from_tactor = target_actor.get_action(compressed_next_states)
-        v_from_tcritic = target_critic.get_state_value(compressed_next_states, na_from_tactor)
-        
-        #calculate yt=r(st,at)+γ⋅Q(st+1,μ(st+1))
-        target_v = compressed_rewards.unsqueeze(1) + GAMMA * v_from_tcritic
-        actual_v = online_critic.get_state_value(compressed_states,compressed_actions)
-        loss = nn.MSELoss()
-        output = loss(actual_v, target_v)
-        optim.zero_grad()
-        output.backward(retain_graph=True)
-        optim.step()
-        
-        online_critic.value_func.zero_grad()
-        
-        for s,a in zip(compressed_states.split(1),compressed_actions.split(1)):
-            online_v = online_critic.get_state_value(s,a)
-            grad_wrt_a = torch.autograd.grad(online_v,(s,a))
-            
-            action = online_actor.get_action(s)
-            action.backward(retain_graph=True)
-            
-            for param in online_actor.policy.parameters():
-                param.data += ALPHA * (param.grad * grad_wrt_a[1].item())/(BATCH_SIZE)
-            
-            writer.add_scalar('Action Gradients', grad_wrt_a[1], i)
-            online_actor.policy.zero_grad()
-            online_critic.value_func.zero_grad()
-        
-        
-#            #soft update
-        
-        for param_o, param_t in zip(online_actor.policy.parameters(), target_actor.policy.parameters()):
-            param_t.data = param_o.data * TAU + param_t.data * (1 - TAU)
-            
-        
-        for param_o, param_t in zip(online_critic.value_func.parameters(), target_critic.value_func.parameters()):
-            param_t.data = param_o.data * TAU + param_t.data * (1 - TAU)
-        
-        online_actor.policy.zero_grad()
-        target_actor.policy.zero_grad()
-        online_critic.value_func.zero_grad()
-        target_critic.value_func.zero_grad()
-        
-        writer.add_scalar('Critic Loss', output, i)
-        writer.add_scalar('Current Reward', reward, i)
-        
+        #get the action using online actor network and add noise for exploration
+        action_d = self.online_actor.get_action(self.state) + self.noise.step()
+        self.act = action_d.mean().unsqueeze(0).unsqueeze(1)
+        #return action to world 
+        return action_d.detach().numpy()
     
-    if i % 5000 == 0:
-        # stats
-#        evalu_steps = 100
-#        state = torch.tensor([env.reset()], dtype=dtype)
-#        cumReward = 0
-#        for st in range(evalu_steps):
-#            a = target_actor.get_action(state)
-#            ns, r, d, _ = env.step([action.detach()])
-#            cumReward += r
-#            state = torch.tensor([ns], dtype=dtype)
-#        
-#        writer.add_scalar('Target Policy Reward', cumReward, i)
+    def agent_train(self, ns, r, done = False):
+        #convert next state and reward to tensors
+        #next_state_v = torch.tensor([next_state],dtype=dtype)
+        #reward_v = torch.tensor([reward],dtype=dtype)
         
-        torch.save(target_actor.policy.state_dict(), 'target_actor_state_1.pt')
-        torch.save(target_critic.value_func.state_dict(), 'target_critic_state_1.pt')
+        #save the values in the replay buffer
+        self.buffer.push(self.state,self.act, r, ns, done)
+        #set the state to the next state to advance agent
+        self.state = ns
+        
+        #if there are enough samples in replay buffer, perform network updates
+        if len(self.buffer) >= self.BUFFER_SIZE:
+            #get a mini batch from the replay buffer
+            sample = self.buffer.sample(self.BATCH_SIZE)
+            #make the data nice
+            compressed_states, compressed_actions, compressed_next_states, compressed_rewards = utils.extract_data(sample)
+            
+            #critic network training
+            #yt=r(st,at)+γ⋅Q(st+1,μ(st+1))
+            na_from_tactor_a = self.target_actor.get_action(compressed_next_states)
+            na_from_tactor = na_from_tactor_a.mean(dim=1).unsqueeze(-1)
+            v_from_tcritic = self.target_critic.get_state_value(compressed_next_states, na_from_tactor)
+            
+            #calculate yt=r(st,at)+γ⋅Q(st+1,μ(st+1))
+            target_v = compressed_rewards.unsqueeze(1) + self.GAMMA * v_from_tcritic
+            actual_v = self.online_critic.get_state_value(compressed_states,compressed_actions)
+            loss = nn.MSELoss()
+            output = loss(actual_v, target_v)
+            self.optim.zero_grad()
+            output.backward(retain_graph=True)
+            self.optim.step()
+            
+            self.online_critic.value_func.zero_grad()
+            
+            for s,a in zip(compressed_states.split(1),compressed_actions.split(1)):
+                online_v = self.online_critic.get_state_value(s,a)
+                grad_wrt_a = torch.autograd.grad(online_v,(s,a))
+                
+                action = self.online_actor.get_action(s)
+                action.mean().backward(retain_graph=True)
+                
+                for param in self.online_actor.policy.parameters():
+                    param.data += self.ALPHA * (param.grad * grad_wrt_a[1].item())/(self.BATCH_SIZE)
+                    
+                self.online_actor.policy.zero_grad()
+                self.online_critic.value_func.zero_grad()
+            
+            
+    #            #soft update
+            
+            for param_o, param_t in zip(self.online_actor.policy.parameters(), self.target_actor.policy.parameters()):
+                param_t.data = param_o.data * self.TAU + param_t.data * (1 - self.TAU)
+                
+            
+            for param_o, param_t in zip(self.online_critic.value_func.parameters(), self.target_critic.value_func.parameters()):
+                param_t.data = param_o.data * self.TAU + param_t.data * (1 - self.TAU)
+            
+            self.online_actor.policy.zero_grad()
+            self.target_actor.policy.zero_grad()
+            self.online_critic.value_func.zero_grad()
+            self.target_critic.value_func.zero_grad()
+            
+            torch.save(self.target_actor.policy.state_dict(), self.agent_name + 'target_actor_state_1.pt')
+            torch.save(self.target_critic.value_func.state_dict(), self.agent_name + 'target_critic_state_1.pt')
         
--- world_tag.py
+++ world_tag.py
@@ -1,21 +1,77 @@
 from make_env import make_env
-import gym
 import torch
-import multiprocessing
+from DDPG_agents import Agent
+from itertools import count
 
 class World():
     def __init__(self, render = False):
-        self.env  = make_env('simple_tag.py')
+        self.env  = make_env('simple_tag')
         self.must_render = render
         
-    def agent_hunt(self):
-        return None
+        self.w_state = self.env.reset()
         
-    def agent_avoid(self):
-        return None
+        self.n_o_prey = self.env.observation_space[0].shape[0]
+        self.n_a_prey = self.env.action_space[0].n
         
+        
+        
+        self.n_o_pred = self.env.observation_space[1].shape[0]
+        self.n_a_pred = self.env.action_space[1].n
+        
+        
+        self.predator_agent = Agent('predator',self.n_o_pred, self.n_a_pred)
+        self.prey_agent = Agent('prey',self.n_o_prey, self.n_a_prey)
+        
+        
+    def split_data(self, states):
+        prey = torch.tensor([states[0]], dtype = torch.float)
+        predator = torch.tensor([states[1]], dtype = torch.float)
+        return prey, predator
+    
+            
+    def act_eval(self, agent):
+        if agent.agent_name == 'predator':
+            state = torch.tensor([self.w_state[1]], dtype = torch.float)
+        else:
+            state = torch.tensor([self.w_state[0]], dtype = torch.float)
+            
+        action = agent.agent_get_action(state)
+        return action
+
     def train(self):
-        return None
+        
+        if self.must_render:
+            self.env.render()
+        
+        prey_action = self.act_eval(self.prey_agent)
+        predator_action = self.act_eval(self.predator_agent)
+        
+        next_state, reward, done, _ = self.env.step([prey_action[0], predator_action[0]])
+        
+        prey_state, predator_state = self.split_data(next_state)
+        
+        prey_reward = torch.tensor([reward[0]], dtype = torch.float)
+        predator_reward = torch.tensor([reward[1]], dtype = torch.float)
+        
+        self.predator_agent.agent_train(predator_state, predator_reward)
+        self.prey_agent.agent_train(prey_state, prey_reward)
+        
+        self.w_state = next_state
+        if done[0] or done[1]:
+            return True
         
         
         
+
+if __name__ == "__main__":
+    w = World(render=True)
+    
+    for i in count():
+        d = w.train()
+        if d:
+            s = w.env.reset()
+            w.w_state = s
+        if i % 100 == 0 and i is not 0:
+            s = w.env.reset()
+            w.w_state = s
+            
